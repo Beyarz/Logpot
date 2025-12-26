9e4329522111e5b6e323495ccee456ea8af5f2af
Beyar <beyar-123+g@hotmail.com>
2025-12-26 18:11:00 +0100

    Added limit on how many lines to print out because IO will bottleneck

--- bin/route.dart
+++ bin/route.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
@@ -6,6 +7,8 @@ import 'package:shelf_router/shelf_router.dart';
 import 'main.dart';
 
 class RouteHandler {
+  static const int _logTailBytes = 64 * 1024 * 1024; // 64MB
+
   final String _robotsTxtFile = 'robots.txt';
   late final String _cacheRobotsTxt;
   late final Router _router;
@@ -29,12 +32,37 @@ class RouteHandler {
 
   Router get router => _router;
 
-  Response _homeHandler(Request req) {
-    return Response.ok('''
-      People and bots have visited following pages:\n\n
-          Level,DateTime,Method,Path\n
-          ${File(logFileName).readAsStringSync()}
-    ''');
+  Future<Response> _homeHandler(Request req) async {
+    final logTail = await _readLogTail();
+
+    final buffer =
+        StringBuffer()
+          ..writeln('People and bots have visited following pages:\n')
+          ..writeln('Level,DateTime,Method,Path\n')
+          ..write(logTail);
+
+    return Response.ok(
+      buffer.toString(),
+      headers: {'Cache-Control': 'no-store'},
+    );
+  }
+
+  Future<String> _readLogTail({int maxLines = 50}) async {
+    final file = File(logFileName);
+    if (!await file.exists()) {
+      return '';
+    }
+
+    final length = await file.length();
+    final start = length > _logTailBytes ? length - _logTailBytes : 0;
+    final rawTail = await file.openRead(start).transform(utf8.decoder).join();
+
+    final lines = const LineSplitter().convert(rawTail);
+    if (lines.length <= maxLines) {
+      return lines.join('\n');
+    }
+
+    return lines.sublist(lines.length - maxLines).join('\n');
   }
 
   Response _healthcheckHandler(Request req) {
